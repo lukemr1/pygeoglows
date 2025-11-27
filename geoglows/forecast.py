59ada620d453087613fbc28d66fee5ab53100512
lukemr1 <lukemroberts123@gmail.com>
2025-11-26 18:08:13 -0700

    created forecast.py and forecast_report function

---
+++ geoglows/forecast.py
@@ -0,0 +1,80 @@
+import plotly.io as pio
+import datetime
+import os
+import io
+pio.kaleido.scope.mathjax = None
+from PyPDF2 import PDFMerger
+from docx import Document
+from docx.shared import Inches
+import tempfile
+import pandas as pd
+from plotly.subplots import make_subplots
+
+from .data import forecast
+from ._plots import plots
+
+today = datetime.date.today()
+
+def _save_plots_to_docx(plots, filename, report_type):
+    """
+    Helper function that accepts plotly figures and outputs a DOCX report.
+    """
+    doc = Document()
+    # Set 8.5 x 11 inches (Letter size)
+    for section in doc.sections:
+        section.page_width = Inches(8.5)
+        section.page_height = Inches(11)
+
+    doc.add_heading('Hydrology Report', 0)
+    doc.add_paragraph(f'Date Generated: {today}')
+    doc.add_paragraph(f'Report Type: {report_type}')
+    doc.add_paragraph('_' * 50)
+    doc.add_paragraph('\n')
+
+    for i, fig in enumerate(plots):
+        # Save fig to bytes as png
+        img_bytes = fig.to_image(format="png", width=1000, height=600, scale=2)
+        img_stream = io.BytesIO(img_bytes)
+
+        # Add to doc
+        doc.add_picture(img_stream, width=Inches(7.0))
+        doc.add_paragraph(f"Figure {i + 1}")
+        doc.add_paragraph("\n")
+
+    # Add Comments Section
+    doc.add_page_break()
+    doc.add_heading('User Comments', level=1)
+    doc.add_paragraph("Please add any observations or notes regarding this report below:")
+    doc.add_paragraph("\n" * 10)
+
+    doc.save(filename)
+    return filename
+
+def forecast_report(riverids=None, user_data=None, date=None):
+
+    figs = []
+
+    if riverids is not None:
+        if date is None:
+            raise ValueError("date is required when providing riverids")
+
+        formatted_date = pd.to_datetime(date).strftime('%Y%m%d')
+
+        for r in riverids:
+            data = forecast(river_id=r, date=formatted_date)
+            fig = plots.forecast(data, plot_titles=["", f"Forecast for River: {r}"])
+            figs.append(fig)
+
+        report_date = date
+
+    elif user_data is not None:
+
+        fig = plots.forecast(user_data, plot_titles=["", f"Forecast for River: {}"])
+        figs.append(fig)
+
+        report_date = date if date else today
+
+    else:
+        raise ValueError("Must provide either 'riverids' or 'data'")
+
+    return _save_plots_to_docx(plots, f"forecast_report_{report_date}", "Forecast Report")
